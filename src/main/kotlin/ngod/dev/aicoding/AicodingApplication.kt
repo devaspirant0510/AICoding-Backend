5311b09dadacb8adc86c7c4d694b3b51366e758b
devaspirant0510 <seungho020510@gmail.com>
2024-11-18 22:09:49 +0900

    feat : 프로젝트 세팅

---
+++ src/main/kotlin/ngod/dev/aicoding/AicodingApplication.kt
@@ -0,0 +1,11 @@
+package ngod.dev.aicoding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AicodingApplication
+
+fun main(args: Array<String>) {
+	runApplication<AicodingApplication>(*args)
+}
---
+++ src/test/kotlin/ngod/dev/aicoding/AicodingApplicationTests.kt
@@ -0,0 +1,13 @@
+package ngod.dev.aicoding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AicodingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
